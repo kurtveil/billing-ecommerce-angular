49b0e8abe8e9b6795d896b031f429ca2e13fe79e
kurt rodriguez <kurt.rodriguez@clave.cl>
2025-08-19 08:37:52 -0500

    update check all products

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -10,10 +10,10 @@ import { CommonModule } from '@angular/common';
 export class Product {
   id: string = '';
   name: string = '';
-  description: string= '';
-  price: string= '';
-  amount: string= '';
-  characteristics: string= '';
+  description: string = '';
+  price: string = '';
+  amount: string = '';
+  characteristics: string = '';
 }
 
 
@@ -26,36 +26,36 @@ export class Product {
   providers: [SnackbarComponent]
 })
 export class ProductsComponent implements OnInit {
-  displayedColumns: string[] = ['select','name', 'description', 'price', 'characteristics', 'amount', 'actions'];
+  displayedColumns: string[] = ['select', 'name', 'description', 'price', 'characteristics', 'amount', 'actions'];
   dataSource = [];
   dialog = inject(MatDialog);
   snackBarCustom = inject(SnackbarComponent);
   selectedProducts = new Set<string>();
-  constructor(public productService: ProductService) { } 
+  constructor(public productService: ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts(){
+  getProducts() {
     this.productService.getProducts().valueChanges.subscribe({
       next: (response: any) => {
         this.dataSource = response.data.products;
         console.log(this.dataSource);
       },
-      error: (error: any) => { 
+      error: (error: any) => {
         console.error(error);
-      }      
+      }
     });
   }
 
 
-  addProduct(){
-    this.openDialogProduct({title:'Agregar Producto'});
+  addProduct() {
+    this.openDialogProduct({ title: 'Agregar Producto' });
   }
 
-  openDialogProduct(data: any){
-    const dialogRef = this.dialog.open(AddProductComponent,{
+  openDialogProduct(data: any) {
+    const dialogRef = this.dialog.open(AddProductComponent, {
       data
     });
 
@@ -64,30 +64,30 @@ export class ProductsComponent implements OnInit {
     });
   }
 
-  editProduct(product: Product){
-    this.openDialogProduct({product: product, title: 'Editar producto'});
+  editProduct(product: Product) {
+    this.openDialogProduct({ product: product, title: 'Editar producto' });
   }
 
-  deleteProduct(product: Product){
+  deleteProduct(product: Product) {
     this.openDialog('100ms', '500ms', product);
   }
 
   openDialog(enterAnimationDuration: string, exitAnimationDuration: string, data: any): void {
     console.log(data);
-    
+
     this.dialog.open(CustomDialogComponent, {
       width: '250px',
       enterAnimationDuration,
       exitAnimationDuration,
       data
-    }).afterClosed().subscribe(result=> {
+    }).afterClosed().subscribe(result => {
       if (result) {
         this.productService.deleteProduct(data.id).subscribe({
-          next: (result)=> {
+          next: (result) => {
             console.log(result);
             this.snackBarCustom.openSnackBar('Producto Eliminado', 'Cerrar', 'center', 'top', 'success-snackbar');
           },
-          error: ()=> {
+          error: () => {
             this.snackBarCustom.openSnackBar('Error', 'Cerrar', 'center', 'top', 'error-snackbar');
           }
         })
@@ -96,25 +96,32 @@ export class ProductsComponent implements OnInit {
   }
 
   toggleSelection(id: string, checked: boolean): void {
-  if (this.selectedProducts.has(id)) {
-    this.selectedProducts.delete(id);
-  } else {
-    this.selectedProducts.add(id);
+    if (this.selectedProducts.has(id)) {
+      this.selectedProducts.delete(id);
+    } else {
+      this.selectedProducts.add(id);
+    }
   }
-}
 
-toggleAllSelection(checked: boolean): void {
-  if (checked) {
-    this.dataSource.forEach((product: Product) => {
-      this.selectedProducts.add(product.id);
-    });
-  } else {
-    this.selectedProducts.clear();
+  toggleAllSelection(checked: boolean): void {
+    if (checked) {
+      this.dataSource.forEach((product: Product) => {
+        this.selectedProducts.add(product.id);
+      });
+    } else {
+      this.selectedProducts.clear();
+    }
   }
-}
 
-deleteProducts(): void {
-  
-}
+  deleteProducts(): void {
 
+  }
+
+  isAllSelected(): boolean {
+    return this.selectedProducts.size === this.dataSource.length;
+  }
+
+  isSomeSelected(): boolean {
+    return this.selectedProducts.size > 0 && !this.isAllSelected();
+  }
 }
